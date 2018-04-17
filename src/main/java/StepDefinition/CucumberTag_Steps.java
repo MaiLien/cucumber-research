ac8c265f97f23dd6019e1cd234470e01a99aaf69
Mai Lien <MaiLien@users.noreply.github.com>
2018-04-17 11:38:52 +0700

    Cucumber Tag

---
+++ src/main/java/StepDefinition/CucumberTag_Steps.java
@@ -0,0 +1,12 @@
+package StepDefinition;
+
+import cucumber.api.java.en.Given;
+
+public class CucumberTag_Steps {
+	
+	@Given("^This is a blank test$")
+	public void isBlank() {
+		//Do nothing
+	}
+
+}
---
+++ src/test/java/toolsqa/cucumber/RunCucumberTagTest.java
@@ -0,0 +1,12 @@
+package toolsqa.cucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = { "Feature/CucumberTag.feature" }, glue = { "StepDefinition" }, monochrome = true, tags = { "@SmokeTest, ~@End2End" })
+public class RunCucumberTagTest {
+
+}
